e351b5d7a0dadf6262afa3c8d80e5128c35dcd1e
melvynrouault <melvyn.rouault@gmail.com>
2023-11-06 18:48:06 +0100

    Change url for mail

--- src/pages/contactPage/ContactPage.jsx
+++ src/pages/contactPage/ContactPage.jsx
@@ -36,7 +36,7 @@ export default function ContactPage() {
         }
         
         if (emailError === null && namesError === null && messageError === null && mailInfos.names && mailInfos.email && mailInfos.message) {
-            await fetch(process.env.API_URL, requestOptions)
+            await fetch("https://backend.madame-paperasse.fr/", requestOptions)
             .then(
                 (data) => {
                     setMailInfos({names: "", email: "", company:"", phoneNumber: "", message: ""})
